aa953342b42d29801123ea0e367438245f44997e
louiszawadzki <louisz@bam.tech>
2019-04-03 12:40:57 +0200

    Send everything

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var wretch = require("wretch");
+var http = require("http");
+var app = express();
+global.fetch = require("node-fetch");
+
+const baseUrl = "http://example.com";
+
+app.get("/images*", function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  var request = http
+    .get(`${baseUrl}${req.originalUrl}`, function(response) {
+      res.writeHead(response.statusCode, {
+        "Content-Type": response.headers["content-type"]
+      });
+      response.pipe(res);
+    })
+    .on("error", function(e) {
+      console.log("Got error: " + e.message, e);
+    });
+});
+
+app.get("*", function(req, res) {
+  res.header("Access-Control-Allow-Origin", "*");
+  return wretch(`${baseUrl}${req.originalUrl}`)
+    .get()
+    .json(result => {
+      res.json(result);
+    });
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
